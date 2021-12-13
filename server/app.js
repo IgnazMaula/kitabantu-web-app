21af5c6e8ba8fe6e587c494342e747112e6cf615
Ignaz Maula <ignazmaula@gmail.com>
2021-12-13 23:12:10 +0800

    error handling unkown routes

--- server/app.js
+++ server/app.js
@@ -3,13 +3,19 @@ const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user-routes');
 const serviceRoutes = require('./routes/service-router');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/users/', userRoutes);
+
 app.use('/api/services/', serviceRoutes);
+
+app.use((req, res, next) => {
+    next(new HttpError('Could not find this route.', 404));
+});
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
