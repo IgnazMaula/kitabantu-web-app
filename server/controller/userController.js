1d7bf97ee93681b71169caec9eca82905015a655
Ignaz Maula <ignazmaula@gmail.com>
2021-12-15 15:48:42 +0800

    login use case

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -95,12 +95,18 @@ const signup = async (req, res, next) => {
     res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
     const { email, password } = req.body;
 
-    const identifiedUser = users.find((u) => u.email === email);
-    if (!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    } catch (error) {
+        return next(new HttpError('Login failed, please try again later', 500));
+    }
+
+    if (!existingUser || existingUser.password !== password) {
+        return next(new HttpError('Failed to login, invalid credential', 401));
     }
 
     res.json({ message: 'Logged in!' });
