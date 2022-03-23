0b0a857af5ff0edf54353204f4bfee66a849b04d
Ignaz Maula <ignazmaula@gmail.com>
2022-03-24 00:47:43 +0800

    index and about us changes

--- client/src/shared/pages/AboutUs.js
+++ client/src/shared/pages/AboutUs.js
@@ -1,13 +1,6 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-const stats = [
-    { label: 'Founded', value: '2021' },
-    { label: 'Employees', value: '5' },
-    { label: 'Beta Users', value: '521' },
-    { label: 'Raised', value: '$25M' },
-];
-
 const AboutUs = () => {
     return (
         <div>
@@ -41,86 +34,24 @@ const AboutUs = () => {
                         </div>
                         <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20'>
                             {/* Testimonial card*/}
-                            <div className='relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden'>
-                                <img
-                                    className='absolute inset-0 h-full w-full object-cover'
-                                    src='https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100'
-                                    alt=''
-                                />
-                                <div className='absolute inset-0 bg-indigo-500 mix-blend-multiply' />
-                                <div className='absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90' />
-                                <div className='relative px-8'>
-                                    <div>
-                                        <img className='h-12' src='https://tailwindui.com/img/logos/workcation.svg?color=white' alt='Workcation' />
-                                    </div>
-                                    <blockquote className='mt-8'>
-                                        <div className='relative text-lg font-medium text-white md:flex-grow'>
-                                            <svg
-                                                className='absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-400'
-                                                fill='currentColor'
-                                                viewBox='0 0 32 32'
-                                                aria-hidden='true'
-                                            >
-                                                <path d='M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z' />
-                                            </svg>
-                                            <p className='relative'>
-                                                Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
-                                                Montes, magna cursus nulla feugiat dignissim id lobortis amet.
-                                            </p>
-                                        </div>
-
-                                        <footer className='mt-4'>
-                                            <p className='text-base font-semibold text-indigo-200'>Sarah Williams, CEO at Workcation</p>
-                                        </footer>
-                                    </blockquote>
-                                </div>
-                            </div>
+                            <img src='/images/banner.png' alt='' />
                         </div>
                     </div>
 
                     <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0'>
                         {/* Content area */}
                         <div className='pt-12 sm:pt-16 lg:pt-20'>
-                            <h2 className='text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl'>On a mission to empower teams</h2>
+                            <h2 className='text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl'>About Kita Bantu</h2>
                             <div className='mt-6 text-gray-500 space-y-6'>
-                                <p className='text-lg'>
-                                    Gentlemen, a short view back to the past. Thirty years ago, Niki Lauda told us ‘take a monkey, place him into the
-                                    cockpit and he is able to drive the car.’ Thirty years later, Sebastian told us ‘I had to start my car like a
-                                    computer, it’s very complicated.’ And Nico Rosberg said that during the race – I don’t remember what race - he
-                                    pressed the wrong button on the wheel. Question for you both: is Formula One driving today too complicated with
-                                    twenty and more buttons on the wheel, are you too much under effort, under pressure? What are your wishes for the
-                                    future concerning the technical programme during the race? Less buttons, more? Or less and more communication with
-                                    your engineers?
+                                <p className='text-base leading-7 text-justify'>
+                                    KitaBantu is a platform for home service workers to offer services to clients. KitaBantu means “We Help”
+                                    (translated from Indonesian), this is because we envision the platform to help home service workers get a wider
+                                    market as well as to help the general public to get home service easily anywhere and anytime. Our project aims to
+                                    provide a platform where workers, especially those who are financially impacted by the COVID-19 pandemic, can
+                                    offer on-demand home services to clients and increase income. We adopt a multi-faceted platform model, which will
+                                    encourage workers to become self-employed enabling them to create their own working conditions and contribute to
+                                    the workers' work-life balance.
                                 </p>
-                                <p className='text-base leading-7'>
-                                    Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu
-                                    vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Sollicitudin tristique eros erat odio sed vitae,
-                                    consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec
-                                    laoreet quam urna.
-                                </p>
-                                <p className='text-base leading-7'>
-                                    Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                                    semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec,
-                                    a. Morbi enim fermentum lacus in. Viverra.
-                                </p>
-                            </div>
-                        </div>
-
-                        {/* Stats section */}
-                        <div className='mt-10'>
-                            <dl className='grid grid-cols-2 gap-x-4 gap-y-8'>
-                                {stats.map((stat) => (
-                                    <div key={stat.label} className='border-t-2 border-gray-100 pt-6'>
-                                        <dt className='text-base font-medium text-gray-500'>{stat.label}</dt>
-                                        <dd className='text-3xl font-extrabold tracking-tight text-gray-900'>{stat.value}</dd>
-                                    </div>
-                                ))}
-                            </dl>
-                            <div className='mt-10'>
-                                <a href='#' className='text-base font-medium text-indigo-600'>
-                                    {' '}
-                                    Learn more about how we're changing the world <span aria-hidden='true'>&rarr;</span>{' '}
-                                </a>
                             </div>
                         </div>
                     </div>
